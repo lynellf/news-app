1cef03d88171dfbb35da2cac0685d0e13230e253
Ezell Frazier <lynellf@gmail.com>
2019-06-25 06:17:59 -0500

    convert getBundle to static method

--- server/controllers/Application.tsx
+++ server/controllers/Application.tsx
@@ -2,13 +2,13 @@ import Main from './Main'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from '../src/views/App'
-import { TRenderRoot } from '../types/controllers/TRenderRoot';
+import { TRenderRoot } from '../types/controllers/Application';
 import { Request } from 'express-serve-static-core';
 
 export default class Application extends Main {
     handleRoot(_req: Request, res: TRenderRoot) {
         const { port } = this
-        const jsBundle = this.getBundle()
+        const jsBundle = Main.getBundle()
         const application = ReactDOMServer.renderToString(<App />)
         res.render('index', { application, jsBundle, port })
     }
